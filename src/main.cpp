55cbe3e454b4583fd29b8448d1057d8731a58309
raph84 <raph.berube@gmail.com>
2020-09-24 19:22:16 -0400

    Cloud relay control mvp

--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,59 @@
 // The MQTT callback function for commands and configuration updates
 // Place your message handler code here.
 #include <Arduino.h>
-
-void messageReceived(String &topic, String &payload) {
-  Serial.println("incoming: " + topic + " - " + payload);
-}
-
 #include "universal-mqtt.h"
 
 #include <Wire.h>
 #include <SPI.h>
 
+#include <ArduinoJson.h>
 
 #define PUBLISH_DELAY 60000
+#define CONTROL_DELAY 10000
 #define RELAY 13
 
 float humidity;
 float temperature;
 unsigned long lastMillis = 0;
-int relay_state = HIGH;
+unsigned long lastMillisControl = 0;
+int relay_state = LOW;
+
+
+
+void update_config(String &topic, String &payload){
+
+  StaticJsonDocument<200> doc;
+
+  DeserializationError error = deserializeJson(doc, payload);
+
+  // Test if parsing succeeds.
+  if (error) {
+    Serial.print(F("deserializeJson() failed: "));
+    Serial.println(error.c_str());
+    return;
+  }
+
+  const bool heating_state = doc["heating_state"];
+  Serial.print("heating_state: ");
+  Serial.println(heating_state ? "TRUE" : "LOW" );
+  if(heating_state){
+    relay_state = HIGH;
+  } else {
+    relay_state = LOW;
+  }
+
+}
+
+void messageReceived(String &topic, String &payload) {
+  Serial.println("incoming: " + topic + " - " + payload);
+
+  if(topic.endsWith("config")){
+    update_config(topic, payload);
+  }
+}
 
 void relay_control() {
-    Serial.println("Relay: " + relay_state );
     digitalWrite(RELAY,relay_state);
-    if(relay_state == HIGH){
-      relay_state = LOW;
-    } else {
-      relay_state = HIGH;
-    }
 }
 
 void setup() {
@@ -49,6 +75,12 @@ void loop() {
     connect();
   }
 
+  if (millis() - lastMillisControl > CONTROL_DELAY) {
+    lastMillisControl = millis();
+    Serial.println("CONTROL");
+    relay_control();
+  }
+
   // publish a message roughly every PUBLISH_DELAY ms.
   if (millis() - lastMillis > PUBLISH_DELAY) {
     lastMillis = millis();
@@ -62,7 +94,7 @@ void loop() {
                      String("}");
     publishTelemetry(payload);
 
-    relay_control();
+    
   }
 
 }
